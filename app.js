fdeca5f8dc2032815c32d389f17ecbddcccc8599
marco cuomo <mar1774co@gmail.com>
2021-10-31 13:58:39 +0100

    [WIP] Server Graphql

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("ciao");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Night Coding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
-app.listen(4000, () => {
-  console.log(`Listening at http://localhost:4000`);
-});
+app.listen(3000);
